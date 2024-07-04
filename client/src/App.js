a705e2dd29f4e25876c675c372c793bb19ad913c
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-04 23:14:54 +0200

    added PrivateRoute.tsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import { createRoutesFromElements,createBrowserRouter,Route,RouterProvider  } from 'react-router-dom'
+import { createRoutesFromElements, createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
 import Login from './features/auth/Login';
 import Home from './pages/Home';
 import Register from './features/auth/Register';
 import PasswordResetRequest from './features/auth/PasswordResetRequest';
 import ResetPassword from './features/auth/ResetPassword';
+import PrivateRoute from './components/PrivateRoute';
 
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route 
+      path="/" 
+      element={
+        <PrivateRoute>
+          <Home />
+        </PrivateRoute>
+      } 
+    />,
+    <Route path="/auth/reset-password" element={<ResetPassword />} />,
+    <Route path="/login" element={<Login />} />,
+    <Route path="/register" element={<Register />} />,
+    <Route path="/request-reset" element={<PasswordResetRequest />} />
+  ])
+);
 
-const router= createBrowserRouter(
-    createRoutesFromElements([
-      <Route path="/" element={<Home />} />,
-        <Route exact path="/auth/reset-password" element={<ResetPassword/>} />,
-        <Route path="/register" element={<Register/>} />,
-        <Route path="/login" element={<Login/>} />,
-        <Route path="/request-reset" element={<PasswordResetRequest/>} />
-    ])
-)
 function App() {
   return (
-   <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   );
 }
 
