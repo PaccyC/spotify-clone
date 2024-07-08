96a9c1be754028d3fb5521565279afedee63b7ec
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-08 13:20:51 +0200

    added styles to reset password page

--- client/src/features/auth/ResetPassword.jsx
+++ client/src/features/auth/ResetPassword.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { resetPassword } from './authSlice';
-
+import spotifylogo from '../../assets/images/spotify-logo.png'
+import { Link } from 'react-router-dom';
 const ResetPassword = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -26,17 +27,52 @@ const ResetPassword = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="password"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-        placeholder="New Password"
-        required
-      />
-      <button type="submit">Reset Password</button>
-    </form>
+    <div className=" min-h-screen bg-black">
+    <nav className=" flex h-36">
+    <img src={spotifylogo} alt="Spotify Logo" />
+    </nav>
+   <div className=" w-full flex justify-center mt-3">
+    <div className=" w-[400px] p-4 space-y-3">
+          <h2 className=" text-3xl text-white font-semibold font-sans">Reset your password</h2>
+          <p className=" text-white ">Please enter your new password below.</p>
+      <form onSubmit={handleSubmit} >
+<label className=' block text-white text-sm font-semibold mb-2'>New Password</label>
+<input
+  type="password"
+  value={newPassword}
+  onChange={(e) => setNewPassword(e.target.value)}
+  required
+  className=" bg-black p-4 border-white w-full text-white border mb-4"
+/>
+<p className='text-white font-semibold text-sm mb-4'>Your password must contain at least</p>
+
+<ul className="list-disc list-inside text-gray-400 space-y-2">
+              <li>10 characters</li>
+              <li>1 letter</li>
+              <li>1 number or special character (example: # ? ! &)</li>
+</ul>
+
+<label className=' block text-white text-sm font-semibold mb-2 mt-6'>Confirm Password</label>
+<input
+  type="password"
+  value={newPassword}
+  onChange={(e) => setNewPassword(e.target.value)}
+  required
+  className=" bg-black p-4 border-white w-full text-white border mb-4"
+/>
+<Link to="https://support.spotify.com/article/reset-password/" className=" text-white underline ">Need support?</Link>
+
+<button
+    type="submit"
+    className=" bg-green-500 w-full rounded-2xl text-black py-3 font-semibold  text-sm mt-4"
+>Change Password</button>
+<button type="submit">Reset Password</button>
+</form>
+  </div>
+  </div> 
+  </div>
   );
 };
 
 export default ResetPassword;
+
